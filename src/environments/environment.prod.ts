d3d377d3d0df57c0bc2ec0a82556e475dc2e9558
KevinMayfield <mayfield.g.kev@gmail.com>
2020-04-01 14:27:34 +0100

    Updated github version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   config : {
-    baseUrl: 'http://fhirserver-env-1.eba-aepmzc4d.eu-west-2.elasticbeanstalk.com:8186/R4'
+    baseUrl: 'https://fhir.test.xgenome.co.uk/R4'
   }
 };
